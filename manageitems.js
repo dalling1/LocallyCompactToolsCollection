6fd21236d9e823827504c72501f25a703967af77
David Allingham <David.Allingham@newcastle.edu.au>
2021-11-01 09:38:29 +1100

    Hides the submission form if an item in the catalogue is clicked on.

--- manageitems.js
+++ manageitems.js
@@ -56,6 +56,8 @@ function setupItems(){
 function showItemDetails(hash){
  // clear any extant hightlighting of the catalogue items
  clearHighlights();
+ // hide the input form
+ hideSubForm();
  // show the details div (in case it is hidden, eg. in the "mobile" layout)
  document.getElementById('details').classList.remove('hidden');
  // look for this item in the hashtable
